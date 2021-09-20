5a36d1bd35fc2387c05936fb743344c9b150c2c9
Avisa-SEI <aap571420@gmail.com>
2021-09-20 09:09:50 -0700

    Fix: create Post

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import dotenv from "dotenv";
 import routes from "./routes/post";
-dotenv.config();
+import mongoose from "mongoose";
+import cors from "cors";
+import PostMessage from "./models/postMessages";
+const app = express();
+require("dotenv").config();
 
 const { PORT, DATABASE_URL } = process.env;
-const app = express();
 
+// mongo connection
 mongoose.Promise = global.Promise;
 mongoose.connect(DATABASE_URL, {
   useNewUrlParser: true,
@@ -19,11 +20,24 @@ mongoose.connect(DATABASE_URL, {
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 
-// cors setup
+// CORS setup
 app.use(cors());
 
 routes(app);
+// app.post("/posts", async (req, res) => {
+//   console.log(req.body);
+//   let newPost = new PostMessage(req.body);
+//   try {
+//     await newPost.save();
+//     res.status(201).json(newPost);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// });
+app.get("/", (req, res) => {
+  res.send("Welcome to Memories App 👩🏻‍💻");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
+  console.log(`Server is listening to ${PORT}`);
 });
