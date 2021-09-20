410025eb32cec15aa8f444494bb20188afbdef59
Avisa-SEI <aap571420@gmail.com>
2021-09-20 16:24:51 -0700

    wip:

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -6,7 +6,7 @@ export const getPosts = async (req, res) => {
     const postMessages = await PostMessage.find();
     res.status(200).json(postMessages);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.status(404).json({ message: error });
   }
 };
 
@@ -16,7 +16,7 @@ export const createPost = async (req, res) => {
     await newPost.save();
     res.status(201).json(newPost);
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
   }
 };
 
