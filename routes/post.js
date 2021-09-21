80347116daaf2cef111c9dec9187ab751facc631
Avisa-SEI <aap571420@gmail.com>
2021-09-20 18:49:53 -0700

    feat: add likecount routes

--- routes/post.js
+++ routes/post.js
@@ -3,11 +3,13 @@ import {
   getPosts,
   updatePost,
   deletePost,
+  likePost,
 } from "../controllers/controllers.js";
 
 const routes = (app) => {
   app.route("/posts").get(getPosts).post(createPost);
   app.route("/posts/:id").patch(updatePost).delete(deletePost);
+  app.route("/posts/:id/likepost").patch(likePost);
 };
 
 export default routes;
